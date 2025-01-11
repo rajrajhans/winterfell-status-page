ea147d5d415813739de9cde18bfbc22c6a0cee34
Raj Rajhans <me@rajrajhans.com>
2025-01-11 21:12:02 +0530

    chore: configure monitors

--- src/config.ts
+++ src/config.ts
@@ -2,42 +2,29 @@ import type { Config } from './types'
 
 export const config: Config = {
   settings: {
-    title: 'Status Page Pro',
-    url: 'https://cf-worker-status-page-pro-production.yunsii.workers.dev',
+    title: 'Winterfell Status',
+    url: 'https://winterfell-status.rajrajhans.com/',
     displayDays: 90,
     collectResponseTimes: true,
   },
   monitors: [
     {
-      id: 'workers.cloudflare.com',
-      url: 'https://workers.cloudflare.com',
-      description: 'You write code. They handle the rest.',
-      followRedirect: false,
-    },
-    {
-      id: 'www.cloudflare.com',
-      url: 'https://www.cloudflare.com',
-      description: 'Built for anything connected to the Internet.',
-    },
-    {
-      id: 'blog.cloudflare.com',
-      url: 'https://blog.cloudflare.com',
-      name: 'The Cloudflare Blog',
-    },
-    {
-      id: 'google',
-      url: 'https://www.google.com/',
-      name: 'Google',
+      id: 'winterfell',
+      name: 'Ping service running on the Winterfell cluster\'s master node',
+      description: 'Ping service running on the Winterfell cluster',
+      url: 'https://winterfell.rajrajhans.com',
+      method: 'GET',
+      expectStatus: 200,
       followRedirect: true,
     },
     {
-      id: 'bilibili',
-      url: 'https://www.bilibili.com/',
-    },
-    {
-      id: 'GitHub',
-      url: 'https://github.com/',
+      id: 'audiobookshelf',
+      name: 'Self hosted Audiobookshelf on the Winterfell cluster',
+      description: 'Audiobookshelf',
+      url: 'https://audiobookshelf.rajrajhans.com',
+      method: 'GET',
+      expectStatus: 200,
+      followRedirect: true,
     },
   ],
-  monitorsCsvUrl: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSnewwW9OuXgtuutyYSfFJ_AZdI-UpkUjP2wWi-zZWM3MKa8IzBceWCe9qB_-Lmk-S7mSFgqKVnokam/pub?gid=0&single=true&output=csv',
 }
